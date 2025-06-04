8550242eb8ef3d5c790247b3d4a8bd132f361268
mark <140616157+pulge@users.noreply.github.com>
2025-06-04 13:43:41 +0800

    setup document program

--- resources/js/pages/document/program.tsx
+++ resources/js/pages/document/program.tsx
@@ -2,10 +2,12 @@ import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
+import { Card } from '@/components/ui/card';
 
 // charts components
 import { DataTable } from "@/components/charts/data-table"
 import data from "../../../../app/Dashboard/data.json"
+import { Car } from 'lucide-react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -19,8 +21,11 @@ export default function Users() {
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-                <div className="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 rounded-xl border md:min-h-min pt-4 pb-4">
-                    <DataTable data={data} />
+                <div className="border-sidebar-border/70 relative min-h-[100vh] flex-1 rounded-xl border md:min-h-min pt-4 pb-4">
+                    <Card>
+                        asd
+                    </Card>
+                    {/* <DataTable data={data} /> */}
                     {/* <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" /> */}
                 </div>
             </div>
--- resources/js/pages/user-management.tsx
+++ resources/js/pages/user-management.tsx
@@ -19,7 +19,7 @@ export default function Users() {
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Users" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-                <div className="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 rounded-xl border md:min-h-min pt-4 pb-4">
+                <div className="border-sidebar-border/70 relative min-h-[100vh] flex-1 rounded-xl border md:min-h-min pt-4 pb-4">
                     <DataTable data={data} />
                     {/* <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" /> */}
                 </div>
