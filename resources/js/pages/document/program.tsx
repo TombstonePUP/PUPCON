2c0d8813ebc1ab49df5f45b1df1cff94271f7b2f
Mark Jason Fulguerinas <markjasonrfulguerinas@iskolarngbayan.pup.edu.ph>
2025-06-05 14:28:40 +0800

    Update program.tsx

--- resources/js/pages/document/program.tsx
+++ resources/js/pages/document/program.tsx
@@ -23,38 +23,48 @@ export default function Users() {
                     </h1>
                 </div>
                 <div className='flex flex-row flex-wrap mt-10 gap-3 justify-center overflow-x-hidden'>
-                    <Link href='/document/program/area' className='border rounded shadow p-7 w-[49.5%]'>
-                        <h1 className='font-black'>Area I</h1>
-                        <p className='text-[#858585]'>Mission, Goals, and Objectives</p>
-                    </Link> <Link href='/document/program/area' className='border rounded shadow p-7 w-[49.5%]'>
-                        <h1 className='font-black'>Area I</h1>
-                        <p className='text-[#858585]'>Mission, Goals, and Objectives</p>
-                    </Link><Link href='/document/program/area' className='border rounded shadow p-7 w-[49.5%]'>
-                        <h1 className='font-black'>Area I</h1>
-                        <p className='text-[#858585]'>Mission, Goals, and Objectives</p>
-                    </Link> <Link href='/document/program/area' className='border rounded shadow p-7 w-[49.5%]'>
-                        <h1 className='font-black'>Area I</h1>
-                        <p className='text-[#858585]'>Mission, Goals, and Objectives</p>
-                    </Link><Link href='/document/program/area' className='border rounded shadow p-7 w-[49.5%]'>
-                        <h1 className='font-black'>Area I</h1>
-                        <p className='text-[#858585]'>Mission, Goals, and Objectives</p>
-                    </Link> <Link href='/document/program/area' className='border rounded shadow p-7 w-[49.5%]'>
-                        <h1 className='font-black'>Area I</h1>
-                        <p className='text-[#858585]'>Mission, Goals, and Objectives</p>
-                    </Link><Link href='/document/program/area' className='border rounded shadow p-7 w-[49.5%]'>
-                        <h1 className='font-black'>Area I</h1>
-                        <p className='text-[#858585]'>Mission, Goals, and Objectives</p>
-                    </Link> <Link href='/document/program/area' className='border rounded shadow p-7 w-[49.5%]'>
-                        <h1 className='font-black'>Area I</h1>
-                        <p className='text-[#858585]'>Mission, Goals, and Objectives</p>
-                    </Link><Link href='/document/program/area' className='border rounded shadow p-7 w-[49.5%]'>
-                        <h1 className='font-black'>Area I</h1>
-                        <p className='text-[#858585]'>Mission, Goals, and Objectives</p>
-                    </Link> <Link href='/document/program/area' className='border rounded shadow p-7 w-[49.5%]'>
+                    <Link href='/document/program/area1' className='border rounded shadow p-7 w-[49.5%]'>
                         <h1 className='font-black'>Area I</h1>
                         <p className='text-[#858585]'>Mission, Goals, and Objectives</p>
                     </Link>
+                    <Link href='/document/program/area2' className='border rounded shadow p-7 w-[49.5%]'>
+                        <h1 className='font-black'>Area II</h1>
+                        <p className='text-[#858585]'>Faculty</p>
+                    </Link>
+                    <Link href='/document/program/area3' className='border rounded shadow p-7 w-[49.5%]'>
+                        <h1 className='font-black'>Area III</h1>
+                        <p className='text-[#858585]'>Curriculum and Instruction</p>
+                    </Link>
+                    <Link href='/document/program/area4' className='border rounded shadow p-7 w-[49.5%]'>
+                        <h1 className='font-black'>Area IV</h1>
+                        <p className='text-[#858585]'>Support to Students</p>
+                    </Link>
+                    <Link href='/document/program/area5' className='border rounded shadow p-7 w-[49.5%]'>
+                        <h1 className='font-black'>Area V</h1>
+                        <p className='text-[#858585]'>Research</p>
+                    </Link>
+                    <Link href='/document/program/area6' className='border rounded shadow p-7 w-[49.5%]'>
+                        <h1 className='font-black'>Area VI</h1>
+                        <p className='text-[#858585]'>Extension and Community Involvement</p>
+                    </Link>
+                    <Link href='/document/program/area7' className='border rounded shadow p-7 w-[49.5%]'>
+                        <h1 className='font-black'>Area VII</h1>
+                        <p className='text-[#858585]'>Library</p>
+                    </Link>
+                    <Link href='/document/program/area8' className='border rounded shadow p-7 w-[49.5%]'>
+                        <h1 className='font-black'>Area VIII</h1>
+                        <p className='text-[#858585]'>Physical Plant and Facilities</p>
+                    </Link>
+                    <Link href='/document/program/area9' className='border rounded shadow p-7 w-[49.5%]'>
+                        <h1 className='font-black'>Area IX</h1>
+                        <p className='text-[#858585]'>Laboratories</p>
+                    </Link>
+                    <Link href='/document/program/area10' className='border rounded shadow p-7 w-[49.5%]'>
+                        <h1 className='font-black'>Area X</h1>
+                        <p className='text-[#858585]'>Administration</p>
+                    </Link>
                 </div>
+
             </div>
         </AppLayout>
     );
