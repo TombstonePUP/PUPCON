a3d8c1a31c56b68b8c28280c12d5d0955dc51aac
1101101011 <ilardecharles@gmail.com>
2025-09-05 21:19:41 +0800

    misc

--- app/Http/Middleware/UserProgramPrivileges.php
+++ app/Http/Middleware/UserProgramPrivileges.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use Symfony\Component\HttpFoundation\Response;
 
 class UserProgramPrivileges
@@ -15,13 +16,14 @@ class UserProgramPrivileges
      */
     public function handle(Request $request, Closure $next): Response
     {
+        $program = Str::of($request->program_name)->replace('_', ' ')->title();
         $userRole = $request->user()?->Roles->first()?->role_name;
 
         if ($userRole === 'Admin' || $userRole === 'Coordinator') {
             return $next($request);
         } elseif ($userRole === 'Chairman') {
             $userProgramRole = $request->user()->Programs;
-            if ($userProgramRole->firstWhere('program_name', $request->program_name)) {
+            if ($userProgramRole->firstWhere('program_name', $program)) {
                 return $next($request);
             }
         }
