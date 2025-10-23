89b06f33bfac8c8ddb8c77a7f90d67ba0549211c
Mark Jason Fulguerinas <markjasonrfulguerinas@iskolarngbayan.pup.edu.ph>
2025-10-24 00:49:57 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,7 +28,8 @@ use App\Models\UserRoles;
 use Illuminate\Database\Seeder;
 use Faker\Factory as Faker;
 use Illuminate\Support\Facades\DB;
-
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\File;
 
 class DatabaseSeeder extends Seeder
 {
@@ -378,97 +379,39 @@ class DatabaseSeeder extends Seeder
         // ExhibitFiles::factory(10)->create();
 
 
-        DB::table('faculties')->insert([
-            [
-                'first_name' => 'Alfeo',
-                'last_name' => 'Mendoza',
-                'faculty_status' => 'Full-Time',
+        $directory = public_path('images/adfa-new'); // folder location
+        $files = File::files($directory); // get all files inside
+
+        $faculties = [];
+
+        foreach ($files as $file) {
+            $fileName = $file->getFilename(); // e.g. "Anna-Madonna-Arellano.jpg"
+            $extension = $file->getExtension(); // e.g. "jpg"
+
+            // remove extension and split by hyphen
+            $nameParts = explode('-', pathinfo($fileName, PATHINFO_FILENAME));
+
+            // handle different name lengths safely
+            if (count($nameParts) >= 2) {
+                $lastName = array_pop($nameParts); // last element = last name
+                $firstName = implode(' ', $nameParts); // rest = first name
+            } else {
+                $firstName = $nameParts[0];
+                $lastName = '';
+            }
+
+            $faculties[] = [
+                'first_name' => Str::title(str_replace('-', ' ', $firstName)),
+                'last_name' => Str::title(str_replace('-', ' ', $lastName)),
+                'faculty_status' => 'Full-Time', // default (can adjust later)
                 'program_id' => 1,
                 'program_coordinator' => false,
-                'faculty_image_name' => 'Alfeo-Mendoza.jpg',
-                'faculty_image_path' => '/images/adfa-new/Alfeo-Mendoza.jpg',
-            ],
-            [
-                'first_name' => 'Alfred',
-                'last_name' => 'Pagalilawan',
-                'faculty_status' => 'Full-Time',
-                'program_id' => 1,
-                'program_coordinator' => false,
-                'faculty_image_name' => 'Alfred-Pagalilawan.jpg',
-                'faculty_image_path' => '/images/adfa-new/Alfred-Pagalilawan.jpg',
-            ],
-            [
-                'first_name' => 'Angeline',
-                'last_name' => 'Pabilona',
-                'faculty_status' => 'Part-Time',
-                'program_id' => 1,
-                'program_coordinator' => false,
-                'faculty_image_name' => 'Angeline-Pabilona.jpg',
-                'faculty_image_path' => '/images/adfa-new/Angeline-Pabilona.jpg',
-            ],
-            [
-                'first_name' => 'Anna Madonna',
-                'last_name' => 'Arellano',
-                'faculty_status' => 'Full-Time',
-                'program_id' => 1,
-                'program_coordinator' => true,
-                'faculty_image_name' => 'Anna-Madonna-Arellano.jpg',
-                'faculty_image_path' => '/images/adfa-new/Anna-Madonna-Arellano.jpg',
-            ],
-            [
-                'first_name' => 'Antonietta',
-                'last_name' => 'Canaon',
-                'faculty_status' => 'Part-Time',
-                'program_id' => 1,
-                'program_coordinator' => false,
-                'faculty_image_name' => 'Antonietta-Canaon.jpg',
-                'faculty_image_path' => '/images/adfa-new/Antonietta-Canaon.jpg',
-            ],
-            [
-                'first_name' => 'Cecilia R.',
-                'last_name' => 'Alagon',
-                'faculty_status' => 'Full-Time',
-                'program_id' => 1,
-                'program_coordinator' => false,
-                'faculty_image_name' => 'Cecilia-R.-Alagon.jpg',
-                'faculty_image_path' => '/images/adfa-new/Cecilia-R.-Alagon.jpg',
-            ],
-            [
-                'first_name' => 'Dianne Marie',
-                'last_name' => 'Villas',
-                'faculty_status' => 'Full-Time',
-                'program_id' => 1,
-                'program_coordinator' => false,
-                'faculty_image_name' => 'Dianne-Marie-Villas.jpg',
-                'faculty_image_path' => '/images/adfa-new/Dianne-Marie-Villas.jpg',
-            ],
-            [
-                'first_name' => 'Elias',
-                'last_name' => 'Austria',
-                'faculty_status' => 'Part-Time',
-                'program_id' => 1,
-                'program_coordinator' => false,
-                'faculty_image_name' => 'Elias-Austria.jpg',
-                'faculty_image_path' => '/images/adfa-new/Elias-Austria.jpg',
-            ],
-            [
-                'first_name' => 'Erwin',
-                'last_name' => 'Dela Cruz',
-                'faculty_status' => 'Full-Time',
-                'program_id' => 1,
-                'program_coordinator' => false,
-                'faculty_image_name' => 'Erwin-Dela-Cruz.jpg',
-                'faculty_image_path' => '/images/adfa-new/Erwin-Dela-Cruz.jpg',
-            ],
-            [
-                'first_name' => 'Imelda',
-                'last_name' => 'Milanez',
-                'faculty_status' => 'Full-Time',
-                'program_id' => 1,
-                'program_coordinator' => false,
-                'faculty_image_name' => 'Imelda-Milanez.jpg',
-                'faculty_image_path' => '/images/adfa-new/Imelda-Milanez.jpg',
-            ],
-        ]);
+                'faculty_image_name' => $fileName,
+                'faculty_image_path' => '/images/adfa-new/' . $fileName,
+            ];
+        }
+
+        // Insert all entries at once
+        DB::table('faculties')->insert($faculties);
     }
 }
