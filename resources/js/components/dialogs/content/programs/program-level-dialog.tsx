d360f9cb3c9ee045faedb320e2b89e8f0cb45a9d
1101101011 <ilardecharles@gmail.com>
2025-12-05 15:22:53 +0800

    misc

--- resources/js/components/dialogs/content/programs/program-level-dialog.tsx
+++ resources/js/components/dialogs/content/programs/program-level-dialog.tsx
@@ -49,7 +49,7 @@ export default function ProgramLevelDialog({ programs, onClose, selected_program
     };
     console.log(programs);
 
-    const programList = programs.filter((p) => !p.under_survey && p.is_active && (p.latest_level?.level ?? -1) < 4 && p.latest_level?.remarks === 'Passed');
+    const programList = programs.filter((p) => !p.under_survey && p.is_active && ((p.latest_level?.level ?? -1) < 4 || p.latest_level?.remarks === 'Passed'));
 
     /* const newLevels = [
         { value: '0', label: 'Preliminary' },
