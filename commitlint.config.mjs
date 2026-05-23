97f627237eb7b9b097e483de54eb40ae4f3d121c
Mark Jason Fulguerinas <markjasonrfulguerinas@iskolarngbayan.pup.edu.ph>
2026-05-23 18:21:31 +0800

    feat: configure Husky git hooks and add commitlint validation

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,11 +1,6 @@
 export default {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        // Max 170 characters for header (subject line)
-        'header-max-length': [2, 'always', 170],
-        // Max 170 characters per body line
-        'body-max-line-length': [2, 'always', 170],
-        // Max 170 characters per footer line
-        'footer-max-line-length': [2, 'always', 170],
-    },
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 170],
+  },
 };
